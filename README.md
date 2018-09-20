b21b18965b24c746815d2d8e1f3a30c0e9561da9
Peter-32 <peter@impactradius.com>
2018-09-20 14:33:42 -0700

    a

--- README.md
+++ README.md
@@ -31,22 +31,60 @@ Run a 10 small random forest times on a univariate time series to predict the ne
 
 ### Steps
 
-- Use a text editor
-- Load the data into Pandas
-- Choose 10 models with the most records
-- Check for missing values / infinity
-- View the ten plots
-- Consider capping at 95% percentiles and/or logging data
-- Optionally cap the data at 95% percentiles
-- View the ten plots
-- Optionally log the data
-- View the ten plots
-- Consider if it looks stationary; regardless don't try to fix it
-- Use last 10 days as the test set
-- Check the average and persistence RMSE
-- Check Auto ARIMA RMSE
-- Check lag 8 Random Forest RMSE
-- Record all results in a pandas then CSV file
-- Print out the prediction for all 10 plots 1.png, ... 10.png.  One line for actual, AUTOArima, and RF
-- Title the plots as the RMSE for each line, and a line for the best RMSE.  Add a legend.  Include a baseline RMSE in this title.
+- ETL
+  - X Use a text editor
+  - X Load the data into pandas
+  - X Format the datetime column
+  - X Create the model key
+  - X Choose 10 models with the most records
+  - X Save to a table
+  - Comment out code
+- Exploration
+  - Load from table
+  - X Excel
+  - X Date as index
+  - X Create a function to view the 10 plots
+  - X Plot all 10
+  - X Check for preferred min/max dates.  Same or different for each time series?
+  - X Consider capping at 95% percentiles
+  - X Consider logging the data
+  - X thoughts on imputing values?
+  - X Check for missing values / infinity
+  - X Comment out code  
+- Prepare Data
+  - X Load from table
+  - X Loop over model keys (break after one iteration for dev)  
+  - X Load model key and drop that column
+  - X Set max and min date
+  - X Fill missing dates and date as index
+  - X Optionally cap the data at 95% percentiles
+  - X Fix missing values or infinity
+  - X View the plot and save it
+  - X Optionally log the data, if so, view the plot and save it
+  - X Consider if it looks stationary (consider seasonality & trend); regardless don't try to fix it for now
+  - X Add the model_key back to the df
+  - X Combine all data
+  - X Plot the 10 plots and save the image
+  - X Save to table
+- Spot Check
+  - Load from table
+  - Date as index
+  - Use last 10 days as the test set
+  - Check the average and persistence RMSE
+  - Check Auto ARIMA RMSE
+  - Check lag 8 Random Forest RMSE
+  - Record all results in a pandas then CSV file
+- Improve
+  - Skip
+- Post
+  - Jupyter Notebook HTML to document the steps (basic, isolation, understandable, & plots)
+  - Basic:
+    - Only focus on the necessary things
+  - Isolation:
+    - Don't reference other projects
+  - Understandable
+    - Make no assumptions about what others know
+  - Plots:
+    - the prediction for all 10 plots 1.png, ... 10.png.  One line for actual, AUTOArima, and RF
+    - Title the plots as the RMSE for each line, and a line for the best RMSE.  Add a legend.  Include a baseline RMSE in this title.
 - End of project
--- main.py
+++ main.py
@@ -1,29 +1,145 @@
 import pandas as pd
-import seaborn as sns
+# import seaborn as sns
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-import missingno as msno
-from sklearn.preprocessing import StandardScaler
+# import missingno as msno
 from pandasql import sqldf
-from sklearn.metrics import mean_squared_error
-from sklearn import linear_model, neighbors, tree, svm, ensemble
-from xgboost import XGBRegressor
-from sklearn.pipeline import make_pipeline
-from tpot.builtins import StackingEstimator
-from sklearn.model_selection import KFold
-from sklearn.model_selection import cross_val_score
-from sklearn.grid_search import GridSearchCV
-from sklearn.pipeline import Pipeline
-from scipy.stats import boxcox
-from scipy.special import inv_boxcox
-pd.set_option('display.width', 5000)
-pd.set_option('display.max_columns', 60)
-%matplotlib inline
+from scipy.stats import mstats
+# from sklearn.preprocessing import StandardScaler
+# from sklearn.metrics import mean_squared_error
+# from sklearn import linear_model, neighbors, tree, svm, ensemble
+# from xgboost import XGBRegressor
+# from sklearn.pipeline import make_pipeline
+# from tpot.builtins import StackingEstimator
+# from sklearn.model_selection import KFold
+# from sklearn.model_selection import cross_val_score
+# from sklearn.grid_search import GridSearchCV
+# from sklearn.pipeline import Pipeline
+# from scipy.stats import boxcox
+# from scipy.special import inv_boxcox
 q = lambda q: sqldf(q, globals())
-mpl.rcParams['figure.figsize'] = (16.0, 16.0)
 
-df = pd.read_csv("daily-total-female-births-in-cal.csv", header=None)
-print(df)
+### FUNCTIONS - ETL
+def do_etl():
+    df = pd.read_csv("source_data/historical_product_demand.csv")
+    df['Date'] = pd.to_datetime(df['Date'])
+    df['Date'] = df['Date'].dt.strftime('%Y-%m-%d')
+    df = sqldf("""SELECT product_code
+                       || "-"
+                       || warehouse
+                       || "-"
+                       || product_category AS model_key,
+                       date,
+                       Sum(IFNULL(order_demand, 0))   AS Order_Demand
+                FROM   df
+                GROUP  BY 1,
+                          2 """, locals())
+    df = sqldf("""SELECT a.*
+                FROM   df a
+                       INNER JOIN (SELECT model_key,
+                                          Count(*) n
+                                   FROM   df
+                                   GROUP  BY 1
+                                   ORDER  BY 2 DESC,
+                                             1
+                                   LIMIT  10) b
+                               ON b.model_key = a.model_key
+                ORDER  BY model_key,
+                          Date """, locals())
+    df.to_csv("tables/1_ten_models.csv", index=False)
 
-# , names = ['CRIM','INDUS','CHAS','NOX','RM','AGE','RAD','TAX','PTRATIO','B','LSTAT','MEDV']
+### FUNCTIONS - Exploration
+def do_exploration():
+    df = pd.read_csv("tables/1_ten_models.csv")
+    df['Date'] = pd.to_datetime(df['Date'])
+    df = df.set_index(["Date"])
+    ten_plots(df=df, filename="1_original_data")
+    # Good min/max dates: 1/5/12 and 12/28/16
+    # Will cap at 95% upper bound and 5% lower bound
+    # Will not log the data
+    # Will impute using 7 day moving average
+    # There are lots of missing values
+    print("There are infinity values: {}".format(df.fillna(0).replace([np.inf, -np.inf], np.nan).isnull().values.any()))
+    # There are no infinity values
+
+def ten_plots(df, filename):
+    df = pd.pivot_table(data=df, values='Order_Demand', index="Date", columns="model_key")
+    df.to_csv("tables/_row_date__col_product.csv")
+    colors=['#A8E6CE', '#A8E6CE',
+            '#DCEDC2', '#DCEDC2',
+            '#FFD3B5', '#FFD3B5',
+            '#FFAAA6', '#FFAAA6',
+            '#FF8C94',  '#FF8C94']
+    fig, ax = plt.subplots(nrows=5, ncols=2)
+    for i, ax in enumerate(ax.flatten()):
+        df[df.columns[i]].plot(color=colors[i], ax=ax, sharex=ax)
+        ax.set_title(df.columns[i], fontsize=8)
+    plt.savefig('images/{}.png'.format(filename))
+
+### FUNCTIONS - Prepare Data
+def do_prepare_data():
+    main_df = pd.read_csv("tables/1_ten_models.csv")
+    model_keys=main_df['model_key'].unique().tolist()
+    prepared_df = pd.DataFrame()
+    for model_key in model_keys:
+        df = load_data(main_df, model_key=model_key)
+        df = fill_missing_days__and_set_datetime_index(df, start_date="2012-01-05", end_date="2016-12-28")
+        df.to_csv("tables/_fill_days.csv")
+        df.loc[:,'Order_Demand'] = mstats.winsorize(df['Order_Demand'].values, limits=[0.05, 0.05])
+        df = moving_average_imputation(df)
+        plot(df, model_key, '2_imputation_example')
+        df['model_key'] = df['Order_Demand'].apply(lambda x: model_key)
+        df['Date'] = df.index
+        prepared_df = pd.concat([prepared_df, df])
+    prepared_df = sqldf("select model_key, Date, Order_Demand from prepared_df ORDER BY 1,2", locals())
+    prepared_df.to_csv("tables/2_data_prepared.csv")
+    prepared_df.set_index(['Date'], inplace=True)
+    ten_plots(df=prepared_df, filename="3_prepared_data")
+
+def load_data(main_df, model_key):
+    return sqldf("SELECT Date, Order_Demand from main_df where model_key = '{}'".format(model_key), locals())
+
+def fill_missing_days__and_set_datetime_index(df, start_date, end_date):
+  idx = pd.date_range(start_date, end_date)
+  df['Date'] = pd.to_datetime(df['Date'])
+  df.set_index('Date', inplace=True)
+  df = df.reindex(idx, fill_value=0)
+  return df
+
+def moving_average_imputation(df):
+    # Consider +/- infinity here if working on a different time series
+    df = df.replace(0, np.nan)
+    df['Order_Demand'] = df['Order_Demand'].fillna(df['Order_Demand'].rolling(window=7, min_periods=1, center=False).mean())
+    df['Order_Demand'] = df['Order_Demand'].fillna(method='ffill')
+    return df
+
+def plot(df, model_key, filename):
+    fig, ax = plt.subplots()
+    ax.set_title(model_key, fontsize=12)
+    plt.plot(df['Order_Demand'])
+    plt.savefig('images/{}.png'.format(filename))
+
+##### MAIN
+
+### ETL
+# do_etl()
+
+### Exploration
+# do_exploration()
+
+### Prepare data
+# do_prepare_data()
+
+
+
+
+
+
+
+
+#
+# print(df.head())
+# print(df.dtypes)
+# print(df.info())
+# print(df.index)
