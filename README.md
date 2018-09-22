c9a9bcf393054e868fb5b9ba9695bfb74f8407bb
Peter-32 <peter@impactradius.com>
2018-09-21 18:56:30 -0700

    a

--- README.md
+++ README.md
@@ -12,84 +12,26 @@ _
 
 ### Goal
 
-Run a 10 small random forest times on each of 30 univariate time series to predict the next 10 days.  Each model predicts one of the days ahead.  Compare the RMSE to AutoARIMA.  View the plots of each
+Compare the RMSE of AutoARIMA with RF for a 10 day forecast.
 
 ### Data Sources
 
-- https://www.kaggle.com/felixzhao/productdemandforecasting/home
-- historical_product_demand.csv
+- https://datamarket.com/data/set/235k/daily-total-female-births-in-california-1959#!ds=235k&display=line
+- daily-total-female-births-in-cal.csv
 
 ### Data Descriptions
 
-- Descriptions from: https://www.kaggle.com/felixzhao/productdemandforecasting/home
-- Product_CodeThe product name encoded
-- WarehouseWarehouse name encoded
-- Product_CategoryProduct Category for each Product_Code encoded
-- DateThe date customer needs the product
-- Order_Demandsingle order qty
-
+- Date: The day without a timestamp
+- Daily total female births in California, 1959
 
 ### Steps
 
-- ETL
-  - X Use a text editor
-  - X Load the data into pandas
-  - X Format the datetime column
-  - X Create the model key
-  - X Choose 10 models with the most records
-  - X Save to a table
-  - Comment out code
-- Exploration
-  - Load from table
-  - X Excel
-  - X Date as index
-  - X Create a function to view the 10 plots
-  - X Plot all 10
-  - X Check for preferred min/max dates.  Same or different for each time series?
-  - X Consider capping at 95% percentiles
-  - X Consider logging the data
-  - X thoughts on imputing values?
-  - X Check for missing values / infinity
-  - X Comment out code  
-- Prepare Data
-  - X Load from table
-  - X Loop over model keys (break after one iteration for dev)  
-  - X Load model key and drop that column
-  - X Set max and min date
-  - X Fill missing dates and date as index
-  - X Optionally cap the data at 95% percentiles
-  - X Fix missing values or infinity
-  - X View the plot and save it
-  - X Optionally log the data, if so, view the plot and save it
-  - X Consider if it looks stationary (consider seasonality & trend); regardless don't try to fix it for now
-  - X Add the model_key back to the df
-  - X Combine all data
-  - X Plot the 10 plots and save the image
-  - X Save to table
+- Start
+  - X Assume it is stationary to start (only go back to this if stuck)
 - Spot Check Algorithms
-  - X Load from table
-  - X Loop over model keys (break after one iteration for dev)  
-  - X Loop over days forecast ahead
-  - X Load model key and drop that column
-  - X Date as index
-  - X Get the y values
-  - X Use last 10 days as the test set
-  - X Chose not to use CV because I don't want to tune the algorithms
-  - X Check the average and persistence MAPE 1 day ahead
-  - Check Auto ARIMA MAPE 1 day ahead
-  - Check lag 8 Random Forest MAPE 1 day ahead
-  - Record all results in a pandas then CSV file
-- Improve
-  - Skip
-- Post
-  - Jupyter Notebook HTML to document the steps (basic, isolation, understandable, & plots)
-  - Basic:
-    - Only focus on the necessary things
-  - Isolation:
-    - Don't reference other projects
-  - Understandable
-    - Make no assumptions about what others know
-  - Plots:
-    - the prediction for all 10 plots 1.png, ... 10.png.  One line for actual, AUTOArima, and RF
-    - Title the plots as the RMSE for each line, and a line for the best RMSE.  Add a legend.  Include a baseline RMSE in this title.
-- End of project
+  - Train/test split last 10 days
+  - Try Auto ARIMA, get the RMSE over 10 days
+  - Try RF
+    - Train 10 models, each X days ahead (copy code)
+    - Use parameters from another project
+    - The test set should be the last available record and 1-10 days ahead (one day prediction for 10 models)
--- main.py
+++ main.py
@@ -5,9 +5,12 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 # import missingno as msno
 from pandasql import sqldf
-from scipy.stats import mstats
+from statsmodels.tsa.arima_model import ARIMA
+# from scipy.stats import mstats
+import statsmodels.api as sm
+from pyramid.arima import auto_arima
 # from sklearn.preprocessing import StandardScaler
-# from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error
 # from sklearn import linear_model, neighbors, tree, svm, ensemble
 # from xgboost import XGBRegressor
 # from sklearn.pipeline import make_pipeline
@@ -20,187 +23,47 @@ from scipy.stats import mstats
 # from scipy.special import inv_boxcox
 q = lambda q: sqldf(q, globals())
 
-### FUNCTIONS - ETL
-def do_etl():
-    df = pd.read_csv("source_data/historical_product_demand.csv")
-    df['Date'] = pd.to_datetime(df['Date'])
-    df['Date'] = df['Date'].dt.strftime('%Y-%m-%d')
-    df = sqldf("""SELECT product_code
-                       || "-"
-                       || warehouse
-                       || "-"
-                       || product_category AS model_key,
-                       date,
-                       Sum(IFNULL(order_demand, 0))   AS Order_Demand
-                FROM   df
-                GROUP  BY 1,
-                          2 """, locals())
-    df = sqldf("""SELECT a.*
-                FROM   df a
-                       INNER JOIN (SELECT model_key,
-                                          Count(*) n
-                                   FROM   df
-                                   GROUP  BY 1
-                                   ORDER  BY 2 DESC,
-                                             1
-                                   LIMIT  30) b
-                               ON b.model_key = a.model_key
-                ORDER  BY model_key,
-                          Date """, locals())
-    df.to_csv("tables/1_thirty_models.csv", index=False)
+days_in_test_set = 10
 
-### FUNCTIONS - Exploration
-def do_exploration():
-    df = pd.read_csv("tables/1_thirty_models.csv")
-    df['Date'] = pd.to_datetime(df['Date'])
-    df = df.set_index(["Date"])
-    thirty_plots(df=df, filename="1_original_data")
-    # Good min/max dates: 1/5/12 and 12/28/16
-    # Will cap at 95% upper bound and 5% lower bound
-    # Will not log the data
-    # Will impute using 7 day moving average
-    # There are lots of missing values
-    print("There are infinity values: {}".format(df.fillna(0).replace([np.inf, -np.inf], np.nan).isnull().values.any()))
-    # There are no infinity values
-
-def thirty_plots(df, filename):
-    df = pd.pivot_table(data=df, values='Order_Demand', index="Date", columns="model_key")
-    df.to_csv("tables/_row_date__col_product.csv")
-    colors=['#A8E6CE', '#A8E6CE', '#A8E6CE',
-            '#DCEDC2', '#DCEDC2', '#DCEDC2',
-            '#FFD3B5', '#FFD3B5', '#FFD3B5',
-            '#FFAAA6', '#FFAAA6', '#FFAAA6',
-            '#FF8C94',  '#FF8C94', '#FF8C94',
-            '#A8E6CE', '#A8E6CE', '#A8E6CE',
-            '#DCEDC2', '#DCEDC2', '#DCEDC2',
-            '#FFD3B5', '#FFD3B5', '#FFD3B5',
-            '#FFAAA6', '#FFAAA6', '#FFAAA6',
-            '#FF8C94',  '#FF8C94', '#FF8C94']
-    fig, ax = plt.subplots(nrows=10, ncols=3)
-    for i, ax in enumerate(ax.flatten()):
-        df[df.columns[i]].plot(color=colors[i], ax=ax, sharex=ax)
-        # ax.set_title(df.columns[i], fontsize=2)
-        ax.tick_params(axis='both', which='major', labelsize=4)
-        ax.tick_params(axis='both', which='minor', labelsize=2)
-    plt.savefig('images/{}.png'.format(filename))
-    plt.close()
-
-### FUNCTIONS - Prepare Data
-def do_prepare_data():
-    main_df = pd.read_csv("tables/1_thirty_models.csv")
-    model_keys=main_df['model_key'].unique().tolist()
-    prepared_df = pd.DataFrame()
-    for model_key in model_keys:
-        df = load_data(main_df=main_df, model_key=model_key)
-        df = fill_missing_days__and_set_datetime_index(df, start_date="2012-01-05", end_date="2016-12-28")
-        df.to_csv("tables/_fill_days.csv")
-        df.loc[:,'Order_Demand'] = mstats.winsorize(df['Order_Demand'].values, limits=[0.05, 0.05])
-        df['Order_Demand'] = df['Order_Demand'].apply(lambda x: np.log1p(x))
-        df = moving_average_imputation(df)
-        plot(df, model_key, '2_imputation_example')
-        df['model_key'] = df['Order_Demand'].apply(lambda x: model_key)
-        df['Date'] = df.index
-        prepared_df = pd.concat([prepared_df, df])
-    prepared_df = sqldf("select model_key, Date, Order_Demand from prepared_df ORDER BY 1,2", locals())
-    prepared_df['Date'] = pd.to_datetime(prepared_df['Date'])
-    prepared_df.to_csv("tables/2_data_prepared.csv", index=False)
-    prepared_df.set_index(['Date'], inplace=True)
-    thirty_plots(df=prepared_df, filename="3_prepared_data")
-
-def load_data(main_df, model_key):
-    return sqldf("SELECT Date, Order_Demand from main_df where model_key = '{}' ORDER BY 1".format(model_key), locals())
-
-def fill_missing_days__and_set_datetime_index(df, start_date, end_date):
-  idx = pd.date_range(start_date, end_date)
-  df['Date'] = pd.to_datetime(df['Date'])
-  df.set_index('Date', inplace=True)
-  df = df.reindex(idx, fill_value=0)
-  return df
-
-def moving_average_imputation(df):
-    # Consider +/- infinity here if working on a different time series
-    df = df.replace(0, np.nan)
-    df['Order_Demand'] = df['Order_Demand'].fillna(df['Order_Demand'].rolling(window=7, min_periods=1, center=False).mean())
-    df['Order_Demand'] = df['Order_Demand'].fillna(method='ffill')
-    return df
-
-def plot(df, model_key, filename):
-    fig, ax = plt.subplots()
-    ax.set_title(model_key, fontsize=12)
-    plt.plot(df['Order_Demand'])
-    plt.savefig('images/{}.png'.format(filename))
-    plt.close()
-
-### FUNCTIONS - Spot Check Algorithms
-
-def do_spot_check_algorithms():
-    pass
-
-def get_train_test_split(df):
-    days_in_test_set = 10
+def get_train_test_split(df, days_in_test_set):
+    days_in_test_set = days_in_test_set
     split_point = len(df) - days_in_test_set
     train, test = df[0:split_point], df[split_point:]
     return train, test
 
-def mean_absolute_percentage_error(y_true, y_pred):
-    y_true, y_pred = np.expm1(y_true), np.expm1(y_pred)
-    return np.mean(np.abs(y_true - y_pred) / np.abs(y_true))
-
-def auto_arima(train):
-    res = sm.tsa.arma_order_select_ic(train['Order_Demand'], ic=['bic'], trend='nc')
-    arima_y_pred = np.ones(10) * 999
-    for d in range(0, 10):
-        try:
-            model = ARIMA(train['amount'], order=(res.bic_min_order[0], d, res.bic_min_order[1]))
-            model = model.fit(disp=0)
-            arima_y_pred = model.forecast(steps=1)[0]
-            break
-        except:
-            pass
-    return res, arima_y_pred
-
-##### MAIN
-
-### ETL
-# do_etl()
-
-### Exploration
-# do_exploration()
-
-### Prepare data
-# do_prepare_data()
-
-### Spot Check Algorithms
-main_df = pd.read_csv("tables/2_data_prepared.csv")
-model_keys=main_df['model_key'].unique().tolist()
-for model_key in model_keys:
-    df = load_data(main_df=main_df, model_key=model_key)
-    df['Date'] = pd.to_datetime(df['Date'])
-    df.set_index('Date', inplace=True)
-    df['y'] = df.shift(-1)
-    df = df.dropna()
-    print(df.head())
-    train, test = get_train_test_split(df)
-    # Chose not to use CV because I don't want to tune the algorithms
-    print("Persistence:")
-    print(mean_absolute_percentage_error(test['y'], test['Order_Demand']))
-    print("Average:")
-    print(mean_absolute_percentage_error(test['y'], np.ones(10) * train['Order_Demand'].mean()))
-    res, arima_y_pred = auto_arima(train)
-    print(arima_y_pred)
+def rmse(y_pred, y_true):
+    return np.sqrt(mean_squared_error(y_pred, y_true))
 
 
+### Improve Algorithm
+# df = pd.read_csv("source_data/daily-total-female-births-in-cal.csv")
+df = pd.read_csv("/Users/peterjmyers/Work/Spot-Check-RF-Vs-AutoARIMA/source_data/daily-total-female-births-in-cal.csv")
+df = df.iloc[0:365]
+df.columns = ['Date', 'x']
+df['Date'] = pd.to_datetime(df['Date'])
+df.index = df['Date']
+df.drop(['Date'], axis=1, inplace=True)
+train, test = get_train_test_split(df, days_in_test_set)
+train, test
 
-    break
 
+stepwise_model = auto_arima(train, start_p=1, start_q=1,
+                           max_p=3, max_q=3, m=12,
+                           start_P=0, seasonal=True,
+                           d=1, D=1, trace=True,
+                           error_action='ignore',
+                           suppress_warnings=True,
+                           stepwise=True)
+print(stepwise_model.aic())
+stepwise_model.fit(train)
+arima_y_pred = stepwise_model.predict(n_periods=days_in_test_set)
+arima_y_pred
 
+print("The AutoARIMA rmse is {}".format(rmse(test['x'], arima_y_pred)))
 
-
-
-
-
-# print(df.shape)
-# print(df.head())
-# print(df.dtypes)
-# print(df.info())
-# print(df.index)
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.plot(test['x'].values)
+ax.plot(arima_y_pred)
+ax.legend(['y_test', 'y_pred'],
+          loc='lower left')
