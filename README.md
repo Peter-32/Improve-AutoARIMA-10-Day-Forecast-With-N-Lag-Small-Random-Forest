42fd6e8a67f9522e7e191fcf45d2cc870622223e
Peter-32 <peter@impactradius.com>
2018-09-21 20:44:33 -0700

    a

--- README.md
+++ README.md
@@ -10,9 +10,13 @@ _
 
 _
 
-### Goal
+### Question
 
-Compare the RMSE of AutoARIMA with RF for a 10 day forecast.
+Is the RF with 8 lags and a small number of components a better model than AutoARIMA for one dataset?
+
+### Hypothesis
+
+RF will have the better score.
 
 ### Data Sources
 
@@ -29,9 +33,14 @@ Compare the RMSE of AutoARIMA with RF for a 10 day forecast.
 - Start
   - X Assume it is stationary to start (only go back to this if stuck)
 - Spot Check Algorithms
-  - Train/test split last 10 days
-  - Try Auto ARIMA, get the RMSE over 10 days
+  - X Train/test split last 10 days
+  - X Try Auto ARIMA, get the RMSE over 10 days
   - Try RF
-    - Train 10 models, each X days ahead (copy code)
-    - Use parameters from another project
-    - The test set should be the last available record and 1-10 days ahead (one day prediction for 10 models)
+    - X lags
+    - X Train 10 models, each X days ahead (copy code)
+    - X Use parameters from another project
+    - X The test set should be the last available record and 1-10 days ahead (one day prediction for 10 models)
+
+### Conclusion
+
+The hypothesis that RF will have a better score was correct.  It outperformed ARIMA drastically on the test set.  Random Forest has an RMSE of 4.04 and ARIMA has an RMSE of 7.64.
--- main.py
+++ main.py
@@ -1,29 +1,19 @@
 import pandas as pd
-# import seaborn as sns
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-# import missingno as msno
 from pandasql import sqldf
 from statsmodels.tsa.arima_model import ARIMA
-# from scipy.stats import mstats
 import statsmodels.api as sm
 from pyramid.arima import auto_arima
-# from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error
-# from sklearn import linear_model, neighbors, tree, svm, ensemble
-# from xgboost import XGBRegressor
-# from sklearn.pipeline import make_pipeline
-# from tpot.builtins import StackingEstimator
-# from sklearn.model_selection import KFold
-# from sklearn.model_selection import cross_val_score
-# from sklearn.grid_search import GridSearchCV
-# from sklearn.pipeline import Pipeline
-# from scipy.stats import boxcox
-# from scipy.special import inv_boxcox
-q = lambda q: sqldf(q, globals())
+from sklearn import ensemble
 
-days_in_test_set = 10
+
+def q(q): return sqldf(q, globals())
+
+# Prepare Data
+
 
 def get_train_test_split(df, days_in_test_set):
     days_in_test_set = days_in_test_set
@@ -31,39 +21,75 @@ def get_train_test_split(df, days_in_test_set):
     train, test = df[0:split_point], df[split_point:]
     return train, test
 
-def rmse(y_pred, y_true):
-    return np.sqrt(mean_squared_error(y_pred, y_true))
 
-
-### Improve Algorithm
-# df = pd.read_csv("source_data/daily-total-female-births-in-cal.csv")
-df = pd.read_csv("/Users/peterjmyers/Work/Spot-Check-RF-Vs-AutoARIMA/source_data/daily-total-female-births-in-cal.csv")
+df = pd.read_csv(
+    "/Users/peterjmyers/Work/Spot-Check-RF-Vs-AutoARIMA/source_data/daily-total-female-births-in-cal.csv")
 df = df.iloc[0:365]
 df.columns = ['Date', 'x']
 df['Date'] = pd.to_datetime(df['Date'])
 df.index = df['Date']
 df.drop(['Date'], axis=1, inplace=True)
+days_in_test_set = 10
 train, test = get_train_test_split(df, days_in_test_set)
 train, test
 
+# Improve Algorithm
 
-stepwise_model = auto_arima(train, start_p=1, start_q=1,
-                           max_p=3, max_q=3, m=12,
-                           start_P=0, seasonal=True,
-                           d=1, D=1, trace=True,
-                           error_action='ignore',
-                           suppress_warnings=True,
-                           stepwise=True)
-print(stepwise_model.aic())
-stepwise_model.fit(train)
-arima_y_pred = stepwise_model.predict(n_periods=days_in_test_set)
-arima_y_pred
+
+def rmse(y_pred, y_true):
+    return np.sqrt(mean_squared_error(y_pred, y_true))
+
+
+def auto_arima_prediction(train, days_in_test_set):
+    stepwise_model = auto_arima(train, start_p=1, start_q=1,
+                                max_p=3, max_q=3, m=12,
+                                start_P=0, seasonal=True,
+                                d=1, D=1, trace=True,
+                                error_action='ignore',
+                                suppress_warnings=True,
+                                stepwise=True)
+    print(stepwise_model.aic())
+    stepwise_model.fit(train)
+    arima_y_pred = stepwise_model.predict(n_periods=days_in_test_set)
+    return arima_y_pred
+
+
+def random_forest_prediction(df, days_in_test_set):
+    for i in range(1, 9):
+        df['lag_{}'.format(i)] = df['x'].shift(i)
+    df = df.dropna()
+    for forecast_days_ahead in range(1, 11):
+        df['y_{}'.format(forecast_days_ahead)
+           ] = df['x'].shift(-1 * forecast_days_ahead)
+    train, test = get_train_test_split(df, days_in_test_set)
+
+    train_X = train[['x', 'lag_1', 'lag_2', 'lag_3',
+                     'lag_4', 'lag_5', 'lag_6', 'lag_7', 'lag_8']]
+    test_X = train.iloc[-1][['x', 'lag_1', 'lag_2', 'lag_3',
+                             'lag_4', 'lag_5', 'lag_6', 'lag_7', 'lag_8']]
+    test_X
+    rf_pred_y = []
+    for days_ahead in range(1, 11):
+        model = ensemble.RandomForestRegressor(
+            n_estimators=50, max_features="log2", min_samples_leaf=5, criterion="mse", bootstrap=True, random_state=2)
+        train_y = train['y_{}'.format(days_ahead)]
+        model = model.fit(train_X, train_y)
+        rf_pred_y.append(model.predict(test_X))
+    rf_pred_y = np.array(rf_pred_y)
+    return rf_pred_y
+
+
+arima_y_pred = auto_arima_prediction(train, days_in_test_set)
+rf_pred_y = random_forest_prediction(df, days_in_test_set)
 
 print("The AutoARIMA rmse is {}".format(rmse(test['x'], arima_y_pred)))
+print("The Random Forest rmse is {}".format(rmse(test['x'], rf_pred_y)))
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 ax.plot(test['x'].values)
 ax.plot(arima_y_pred)
-ax.legend(['y_test', 'y_pred'],
+ax.plot(rf_pred_y)
+
+ax.legend(['y_test', 'arima_y_pred', 'rf_pred_y'],
           loc='lower left')
